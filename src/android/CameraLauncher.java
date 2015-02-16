ee83a7db4de3f4179fce90062cda8b9b4c8a967d
Henri <hchevallier@sharinglabs.com>
2015-02-16 18:59:32 +0800

    - Fixed: forgot import

--- src/android/CameraLauncher.java
+++ src/android/CameraLauncher.java
@@ -25,6 +25,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.channels;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
