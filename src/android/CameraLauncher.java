cc8395af9bcdbea05ba193d52094cd2c7bab171c
Henri <hchevallier@sharinglabs.com>
2015-02-17 09:05:28 +0800

    - Fix compilation issue

--- src/android/CameraLauncher.java
+++ src/android/CameraLauncher.java
@@ -476,27 +476,23 @@ private String ouputModifiedBitmap(Bitmap bitmap, Uri uri) throws IOException {
     }
 
 
-private void copyFile(File sourceFile, File destFile) throws IOException {
-	try {
-		if (!sourceFile.exists()) {
-			return;
-		}
+private void copyFile(File sourceFile, File destFile) {
+	if (!sourceFile.exists()) {
+		return;
+	}
 
-		FileChannel source = null;
-		FileChannel destination = null;
-		source = new FileInputStream(sourceFile).getChannel();
-		destination = new FileOutputStream(destFile).getChannel();
-		if (destination != null && source != null) {
-			destination.transferFrom(source, 0, source.size());
-		}
-		if (source != null) {
-			source.close();
-		}
-		if (destination != null) {
-			destination.close();
-		}
-	} catch (IOException e) {
-		
+	FileChannel source = null;
+	FileChannel destination = null;
+	source = new FileInputStream(sourceFile).getChannel();
+	destination = new FileOutputStream(destFile).getChannel();
+	if (destination != null && source != null) {
+		destination.transferFrom(source, 0, source.size());
+	}
+	if (source != null) {
+		source.close();
+	}
+	if (destination != null) {
+		destination.close();
 	}
 }
 	
